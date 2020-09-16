93a3e39c027c547c3759412b9fb08be471cb6c77
Patorik <p4t0r1k@yahoo.com>
2020-09-16 11:41:04 +0200

    Modified software to satisfy conditions.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <string>
 #include "character.h"
-#include "game.h"
 
-int main(){
-    Game game;
-    game.start();
+int main(int argc, char* argv[]){
+    using std::stoi;
+    using std::cout;
+    Character player(argv[1], stoi(argv[2]), stoi(argv[3]));
+    Character enemy(argv[4], stoi(argv[5]), stoi(argv[6]));
+    player.print();
+    enemy.print();
+    while(enemy.isAlive() && player.isAlive()){
+        player.attack(enemy);
+        player.print();
+        enemy.print();
+        // Enemy dead
+        if(!enemy.isAlive()){
+            cout << enemy.getName() << " died. " << player.getName() << " wins.\n";
+            break;
+        }
+        enemy.attack(player);
+        player.print();
+        enemy.print();
+        // Player dead
+        if(!player.isAlive()){
+            cout << player.getName() << " died. " << enemy.getName() << " wins.\n";
+        }
+    }
     return 0;
 }
