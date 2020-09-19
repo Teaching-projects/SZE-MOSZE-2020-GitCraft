6623a9a0185979a5966e8f2584b147783996abab
Tamas Zelles <zelles.tamas@gmail.com>
2020-09-19 20:54:25 +0200

    print problems fixed

--- main.cpp
+++ main.cpp
@@ -2,33 +2,32 @@
 #include <string>
 #include "character.h"
 
-int main(int argc, char* argv[]){
-    using std::stoi;
-    using std::cout;
-    try{
-        Character player(argv[1], stoi(argv[2]), stoi(argv[3]));
-        Character enemy(argv[4], stoi(argv[5]), stoi(argv[6]));
-        player.print();
-        enemy.print();
-        while(enemy.isAlive() && player.isAlive()){
-            player.attack(enemy);
-            cout << player;
-            cout << enemy;
-            // Enemy dead
-            if(!enemy.isAlive()){
-                cout << enemy.getName() << " died. " << player.getName() << " wins.\n";
-                break;
-            }
-            enemy.attack(player);
-            cout << player;
-            cout << enemy;
-            // Player dead
-            if(!player.isAlive()){
-                cout << player.getName() << " died. " << enemy.getName() << " wins.\n";
-            }
-        }
-    }catch(std::invalid_argument &ia){
-        std::cerr << "Invalid argument\n";
-    }
-    return 0;
+int main(int argc, char* argv[]) {
+	using std::stoi;
+	using std::cout;
+	try {
+		Character player(argv[1], stoi(argv[2]), stoi(argv[3]));
+		Character enemy(argv[4], stoi(argv[5]), stoi(argv[6]));
+		while (enemy.isAlive() && player.isAlive()) {
+			player.attack(enemy);
+			cout << player;
+			cout << enemy;
+			// Enemy dead
+			if (!enemy.isAlive()) {
+				cout << enemy.getName() << " died. " << player.getName() << " wins.\n";
+				break;
+			}
+			enemy.attack(player);
+			cout << player;
+			cout << enemy;
+			// Player dead
+			if (!player.isAlive()) {
+				cout << player.getName() << " died. " << enemy.getName() << " wins.\n";
+			}
+		}
+	}
+	catch (std::invalid_argument &ia) {
+		std::cerr << "Invalid argument\n";
+	}
+	return 0;
 }
