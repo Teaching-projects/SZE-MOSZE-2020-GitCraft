686e7425797192f0fb8bc99e54079ea283554446
Patorik <p4t0r1k@yahoo.com>
2020-09-23 22:41:10 +0200

    Fixed missing >> operator in test.sh

--- test.sh
+++ test.sh
@@ -2,9 +2,9 @@
 
 IFS=$"\n"
 rm output.txt
-echo `./main Orc.json Elf.json`
-echo `./main Elf.json Orc.json`
-echo `./main Orc.json Troll.json`
-echo `./main Troll.json Orc.json`
-echo `./main Elf.json Troll.json`
-echo `./main Troll.json Elf.json`
+echo `./main Orc.json Elf.json` >> $2
+echo `./main Elf.json Orc.json` >> $2
+echo `./main Orc.json Troll.json` >> $2
+echo `./main Troll.json Orc.json` >> $2
+echo `./main Elf.json Troll.json` >> $2
+echo `./main Troll.json Elf.json` >> $2
