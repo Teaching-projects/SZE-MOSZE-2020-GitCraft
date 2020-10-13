1e5d5be1a52a666e47b385e5912571eba42c3c81
Tamas Zelles <zelles.tamas@gmail.com>
2020-10-13 18:32:28 +0200

    sytax fix run_valgrind_check.sh

--- run_valgrind_check.sh
+++ run_valgrind_check.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 IFS=$'\n'
-echo "$(valgrind --leak-check=yes --log-file=valgrind_check.txt ./a.out Troll.json Elf.json)"
+echo "$(valgrind --leak-check=yes --log-file=valgrind_check.txt ./a.out Troll.json Orc.json)"
 valgrind_check_result="$(cat ./valgrind_check.txt)"
 echo $valgrind_check_result
-memory_leak_errors="$(echo $valgrind_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
-if ["$memory_leak_errors" == "0"]
+check_errors="$(echo $valgrind_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
+if [ "$check_errors" == "0" ]
 then
     echo "No memory leaks!"
-
 else
-    echo "Memory leaks found!"
+    echo "Memory leaks!"
     exit 1
 fi
