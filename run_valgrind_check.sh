e62986c70253e2d14021d0fe419b5c143a6701ad
Tamas Zelles <zelles.tamas@gmail.com>
2020-10-13 18:29:03 +0200

    fix ws problems run_valgrind_check.sh

--- run_valgrind_check.sh
+++ run_valgrind_check.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 IFS=$'\n'
-
 echo "$(valgrind --leak-check=yes --log-file=valgrind_check.txt ./a.out Troll.json Elf.json)"
 valgrind_check_result="$(cat ./valgrind_check.txt)"
 echo $valgrind_check_result
-
 memory_leak_errors="$(echo $valgrind_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
-
 if ["$memory_leak_errors" == "0"]
 then
     echo "No memory leaks!"
