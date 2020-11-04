d180f8d52241091a8d3c6c71a44dea2e5b111a0d
Tamas Zelles <zelles.tamas@gmail.com>
2020-11-04 21:09:00 +0100

    modify chraracter and preaper to inheritance

--- character.cpp
+++ character.cpp
@@ -1,13 +1,10 @@
 #include "character.h"
 #include <iostream>
-#include <fstream>
-#include <cmath>
 #include <string>
 
 
 Character::Character(const std::string& name, const int maxHp, const int dmg, double attackcooldown) : name(name), maxHp(maxHp), damage(dmg), attack_cooldown(attackcooldown)
 {
-	health_points = maxHp;
 }
 std::string Character::getName() const
 {
@@ -33,31 +30,6 @@ int Character::getMaxHealthPoints() const
 	return maxHp;
 }
 
-int Character::getXp() const
-{
-	return xp;
-}
-
-int Character::getLevel() const
-{
-	return level;
-}
-
-int Character::getExpPerLevel() const
-{
-	return exp_per_level;
-}
-
-int Character::getHealthPerLevel() const
-{
-	return health_per_level;
-}
-
-int Character::getDamageBonus() const
-{
-	return damage_bonus;
-}
-
 
 bool Character::isAlive() const
 {
@@ -71,80 +43,7 @@ bool Character::isAlive() const
 	}
 }
 
-void Character::fightTilDeath(Character &c)
-{
-	if (c.isAlive())
-	{
-		this->attack(c);
-		this->levelup();
-	}
-}
-
-void Character::attack(Character &player)
-{
-	int act_xp = 0;
-	if (player.getHealthPoints() - getDamage() > 0)
-	{
-		player.health_points -= getDamage();
-		act_xp = getDamage();
-	}
-	else
-	{
-		act_xp = player.getHealthPoints();
-		player.health_points = 0;
-	}
-	xp += act_xp;
-}
-
-void Character::levelup()
-{
-	int level_c = getXp() / 100;
-	for (int i = 0; i < level_c; i++)
-	{
-		level++;
-		damage += round(getDamage()*0.1);
-		maxHp += round(getMaxHealthPoints()*0.1);
-		health_points = maxHp;
-		xp -= 100;
-		attack_cooldown-= round(getAttackCoolDown()*0.1);
-	}
-}
-Character* Character::takeDamage(Character& player, Character& enemy)
-{
-	double t1=0.0;
-	double t2=0.0;
-	while(enemy.isAlive() && player.isAlive()){
-		if(t1<t2){
-			player.fightTilDeath(enemy);
-			if(!enemy.isAlive()){
-				return &player;
-			}
-			t1+=player.attack_cooldown;
-		}
-		else if(t1>t2){
-			enemy.fightTilDeath(player);
-			if(!player.isAlive()){
-				return &enemy;
-			}
-			t2+=enemy.attack_cooldown;
-		}
-		else{
-			player.fightTilDeath(enemy);
-			if (!enemy.isAlive()){
-				return &player;
-			}
-			t1+=player.attack_cooldown;
-			enemy.fightTilDeath(player);
-			if(!player.isAlive()){
-				return &enemy;
-			}
-			t2+=enemy.attack_cooldown;
-		}
-	}
-	return nullptr;
-}
-
 std::ostream & operator<<(std::ostream & os, const Character &C) {
-	os << C.getName() << ": HP: " << C.getHealthPoints() << ", MaxHP:" << C.getMaxHealthPoints() << ", DMG: " << C.getDamage() << ", XP: " << C.getXp() << ", Level: " << C.getLevel() << '\n';
+	os << C.getName() << ": HP: " << C.getHealthPoints() << ", MaxHP:" << C.getMaxHealthPoints() << ", DMG: " << C.getDamage() << ", XP: " <<'\n';
 	return os;
 }
--- character.h
+++ character.h
@@ -23,19 +23,8 @@ protected:
     int maxHp;///< This is the health points of the character.
     int damage;///< This is the damage of the character.
     int health_points;///< This is the actual health of the caracter.
-    int xp = 0;///< This is the experience of the character.
-	int level = 1;///< This is the level of the character.
     double attack_cooldown;/// < This is the attackspeed.
-    int exp_per_level;/// < This is the experience the hero needs for level up variable.
-    int health_per_level;/// < This is the health the character gets after level up.
-    int damage_bonus;/// < This is the value of the damage bonus.
-    /// This function manage the level ups of the characters.
-    void levelup(/**< [in] There's no parameter here.*/);
-    /// This function manages the healt points of the characters in a fight.
-    void attack(Character &c/** [in] This is the created character*/);
 public:
-    Character(){};
-    /// This constructor is setting up the private variables.
     Character(const std::string& name/** [in] The name of the character.*/, const int maxHp/** [in] The healt points of the character*/, const int dmg/** [in] The damage of the character*/, double attackcooldown/** [in] The attackspeed of the character*/);
     /// This constructor is return the name of the character.
     std::string getName(/**< [in] There's no parameter here.*/) const;
@@ -45,27 +34,10 @@ public:
     int getDamage(/**< [in] There's no parameter here.*/) const;
     /// This constructor is return the actual hp of the character.
     int getMaxHealthPoints(/**< [in] There's no parameter here.*/) const;
-    /// This constructor is return with the experience of the character.
-    int getXp(/**< [in] There's no parameter here.*/) const;
-    /// This contructor return the level of the character.
-	int getLevel(/**< [in] There's no parameter here.*/) const;
     /// This constructor is return the attackspeed of the character.
     double getAttackCoolDown(/**< [in] There's no parameter here.*/) const;
     /// This function is chechking the hp of the characters. If it's 0, return false, else true.
-    /// This function is a getter for the experiences of the character to increase the hp.
-    int getExpPerLevel(/**< [in] There's no parameter here.*/) const;
-    /// This function is a getter for the health points.
-    int getHealthPerLevel(/**< [in] There's no parameter here.*/) const;
-    /// This funciton is a getter for the damage.
-    int getDamageBonus(/**< [in] There's no parameter here.*/) const;
-    /// This function is chechking the hp of the characters. If it's 0, return false, else true.
     bool isAlive(/**< [in] There's no parameter here.*/) const;
-    /// This function is the fight method.
-    void fightTilDeath(Character &c/** [in] This is the created character*/);
-    /// This function is making the fights, using in main.
-    Character* takeDamage(Character& player/** [in] This character is the first fighter*/, Character& enemy/** [in] This character is the second fighter*/);
-	///This function manages the input datas from the .json file.
-    static Character parse(std::string& charSheetName/** [in] This is the name of the file*/);
     /// This is the friend of the class and overload the "cout".
     friend std::ostream & operator<<(std::ostream & os/** [in] This is an ostream referenc to the output*/, const Character &C/** [in] This is what the program print out*/);
 };
