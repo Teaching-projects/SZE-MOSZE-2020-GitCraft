3c142d63c3ca4b0ffc649ac5699f8d3f3444c665
Tamas Zelles <zelles.tamas@gmail.com>
2020-11-05 02:45:56 +0100

    modify in-out tests

--- test.sh
+++ test.sh
@@ -4,9 +4,5 @@ IFS=$'\n'
 
 rm output.txt
 
-echo `./main Orc.json Elf.json` >> $2
-echo `./main Elf.json Orc.json` >> $2
-echo `./main Orc.json Troll.json` >> $2
-echo `./main Troll.json Orc.json` >> $2
-echo `./main Elf.json Troll.json` >> $2
-echo `./main Troll.json Elf.json` >> $2
+echo `./main scenario1.json` >> $2
+echo `./main scenario2.json` >> $2
