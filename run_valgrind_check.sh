68af40fb4a61cf5ef82f29e889f35884baefe210
Tamas Zelles <zelles.tamas@gmail.com>
2020-12-05 19:34:02 +0100

    modify valgrind chechk

--- run_valgrind_check.sh
+++ run_valgrind_check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 IFS=$'\n'
-echo "$(valgrind --leak-check=yes --log-file=valgrind_check.txt ./main scenario1.json)"
+echo "$(valgrind --leak-check=yes --log-file=valgrind_check.txt ./main sc_test.json < testinput.txt)"
 valgrind_check_result="$(cat ./valgrind_check.txt)"
 echo $valgrind_check_result
 check_errors="$(echo $valgrind_check_result | sed 's/^.*ERROR SUMMARY: \([0-9]*\) errors.*$/\1/')"
