e47f49a805a58ff5344d0f32bbb4e53a778294b8
Tamas Zelles <zelles.tamas@gmail.com>
2020-12-05 21:04:52 +0100

    modify test script

--- test.sh
+++ test.sh
@@ -4,4 +4,5 @@ IFS=$'\n'
 
 rm output.txt
 
-echo `./main sc_test.json < testinput.txt` >> output.txt
+#echo `./main sc_test.json < testinput.txt` >> output.txt
+cat testinput.txt | ./main >> output.txt
