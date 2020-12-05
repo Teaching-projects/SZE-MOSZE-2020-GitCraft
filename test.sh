474d8f34a957ffbc7203b00839dc902c5b0461c0
Tamas Zelles <zelles.tamas@gmail.com>
2020-12-05 21:13:58 +0100

    modify test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-IFS=$'\n'
+#IFS=$'\n'
 
-rm output.txt
+#rm output.txt
 
 #echo `./main sc_test.json < testinput.txt` >> output.txt
-cat testinput.txt | ./main sc_test.json >> output.txt
+#cat testinput.txt | ./main sc_test.json >> output.txt
+
+
+FILE=output.txt
+INPF=sc_test.json
+
+if test -f $FILE
+then
+	rm $FILE
+fi
+
+cat testinput.txt | ./main >> $FILE
