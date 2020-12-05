187d4538a1bdc212b771a66af1cd8d5fc21185b6
Tamas Zelles <zelles.tamas@gmail.com>
2020-12-05 22:10:46 +0100

    modify test script

--- test.sh
+++ test.sh
@@ -2,6 +2,12 @@
 
 #IFS=$'\n'
 
-rm output.txt
+#rm output.txt
 
-echo `./main sc_test.json < testinput.txt` >> output.txt
+#echo `./main sc_test.json < testinput.txt` >> output.txt
+
+IFS=$'\n'
+rm output.txt
+for i in `cat testinput.txt`; do
+    echo "$i" | `./main sc_test.json` >> output.txt
+done
