06ebc373a3f776a6a9f900b441e78358989d20fe
Tamas Zelles <zelles.tamas@gmail.com>
2020-12-06 23:56:04 +0100

    modify test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 
 #IFS=$'\n'
 #echo `./main test PreparedGame.json` > io_test.txt
-echo `./main prepared PreparedGame.json < testinput.txt` > io_test.txt
+#echo `./main prepared PreparedGame.json < testinput.txt` > io_test.txt
+echo `printf 'west south south east east south south east east east west north north east north east' | ./main prepared PreparedGame.json` > io_test.txt
